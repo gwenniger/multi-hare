Fixed the multi-directional computation for MultiDimensional RNN. Added a centralized method for computing the output feature size computed by the network, i.e. the size of the flattened list of all activation values for all positions i,j in the images combined.
	modified:   modules/multi_dimensional_lstm.py
	modified:   modules/multi_dimensional_rnn.py
	modified:   modules/train_multi_dimensional_rnn.py

modules/multi_dimensional_lstm.py
@@ -16,17 +16,17 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
         # Input
         self.input_input_convolution = nn.Conv2d(self.input_channels,
                                                  self.hidden_states_size, 1)
-        self.input_hidden_state_convolution = nn.Conv1d(self.input_channels,
+        self.input_hidden_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                         self.hidden_states_size, 2,
                                                         padding=1)
 
         # Input gate
         self.input_gate_input_convolution = nn.Conv2d(self.input_channels,
                                                       self.hidden_states_size, 1)
-        self.input_gate_hidden_state_convolution = nn.Conv1d(self.input_channels,
+        self.input_gate_hidden_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                              self.hidden_states_size, 2,
                                                              padding=1)
-        self.input_gate_memory_state_convolution = nn.Conv1d(self.input_channels,
+        self.input_gate_memory_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                              self.hidden_states_size, 2,
                                                              padding=1)
 
@@ -36,11 +36,11 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
         self.forget_gate_one_input_convolution = nn.Conv2d(self.input_channels,
                                                            self.hidden_states_size, 1)
         self.forget_gate_one_input_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
-        self.forget_gate_one_hidden_state_convolution = nn.Conv1d(self.input_channels,
+        self.forget_gate_one_hidden_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                                   self.hidden_states_size, 2,
                                                                   padding=1)
         self.forget_gate_one_hidden_state_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
-        self.forget_gate_one_memory_state_convolution = nn.Conv1d(self.input_channels,
+        self.forget_gate_one_memory_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                                   self.hidden_states_size, 1)
         self.forget_gate_one_memory_state_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
 
@@ -49,31 +49,29 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
         self.forget_gate_two_input_convolution = nn.Conv2d(self.input_channels,
                                                            self.hidden_states_size, 1)
         self.forget_gate_two_input_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
-        self.forget_gate_two_hidden_state_convolution = nn.Conv1d(self.input_channels,
+        self.forget_gate_two_hidden_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                                   self.hidden_states_size, 2,
                                                                   padding=1)
         self.forget_gate_two_hidden_state_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
-        self.forget_gate_two_memory_state_convolution = nn.Conv1d(self.input_channels,
+        self.forget_gate_two_memory_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                                   self.hidden_states_size, 1)
         self.forget_gate_two_memory_state_convolution.bias.data.fill_(FORGET_GATE_BIAS_INIT)
 
         # Output gate
         self.output_gate_input_convolution = nn.Conv2d(self.input_channels,
                                                        self.hidden_states_size, 1)
-        self.output_gate_hidden_state_convolution = nn.Conv1d(self.input_channels,
+        self.output_gate_hidden_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                               self.hidden_states_size, 2,
                                                               padding=1)
-        self.output_gate_memory_state_convolution = nn.Conv1d(self.input_channels,
+        self.output_gate_memory_state_convolution = nn.Conv1d(self.hidden_states_size,
                                                               self.hidden_states_size, 2,
-                                                              padding = 1)
+                                                              padding=1)
 
-
-        # self.fc3 = nn.Linear(1024, 10)
         # For multi-directional rnn
         if self.compute_multi_directional:
-            self.fc3 = nn.Linear(1024 * 4, 10)
+            self.fc3 = nn.Linear(self.number_of_output_dimensions(), 10)
         else:
-            self.fc3 = nn.Linear(1024, 10)
+            self.fc3 = nn.Linear(self.number_of_output_dimensions(), 10)
 
     @staticmethod
     def create_multi_dimensional_lstm(batch_size, compute_multi_directional: bool,

modules/multi_dimensional_rnn.py
@@ -218,7 +218,7 @@ class MultiDimensionalRNNBase(torch.nn.Module):
         #    activations_rearranged = torch.cat((activations_rearranged, activations_rearranged_row), 0)
 
         # print("activations_rearranged: " + str(activations_rearranged))
-        activations_one_dimensional = activations_selected.view(-1, 32 * 32 * 4)
+        activations_one_dimensional = activations_selected.view(-1, self.number_of_output_dimensions())
         # activations_one_dimensional = activations_rearranged.view(-1, 32 * 32 * 4)
 
         # print("activations_combined: " + str(activations_combined))
@@ -341,6 +341,13 @@ class MultiDimensionalRNNBase(torch.nn.Module):
         # print("state_plus_input: " + str(state_plus_input))
         return state_plus_input
 
+    def number_of_output_dimensions(self):
+        result = 1024 * self.hidden_states_size
+        if self.compute_multi_directional:
+            result = result * 4
+        return result
+
+
 
 class MultiDimensionalRNN(MultiDimensionalRNNBase):
     def __init__(self, hidden_states_size, batch_size, compute_multi_directional: bool,
@@ -356,9 +363,11 @@ class MultiDimensionalRNN(MultiDimensionalRNNBase):
         # self.fc3 = nn.Linear(1024, 10)
         # For multi-directional rnn
         if self.compute_multi_directional:
-            self.fc3 = nn.Linear(1024 * 4 * self.hidden_states_size, 10)
+            self.fc3 = nn.Linear(self.number_of_output_dimensions(), 10)
         else:
-            self.fc3 = nn.Linear(1024 * self.hidden_states_size, 10)
+            self.fc3 = nn.Linear(self.number_of_output_dimensions(), 10)
+
+
 
     @staticmethod
     def create_multi_dimensional_rnn(hidden_states_size: int, batch_size: int,  compute_multi_directional: bool,
@@ -424,7 +433,7 @@ class MultiDimensionalRNN(MultiDimensionalRNNBase):
 
     def forward_one_directional_multi_dimensional_rnn(self, x):
         activations_unskewed = self.compute_multi_dimensional_rnn_one_direction(x)
-        activations_one_dimensional = activations_unskewed.view(-1, 1024 * self.hidden_states_size)
+        activations_one_dimensional = activations_unskewed.view(-1, self.number_of_output_dimensions())
         # print("activations_one_dimensional: " + str(activations_one_dimensional))
         # It is nescessary to output a tensor of size 10, for 10 different output classes
         result = self.fc3(activations_one_dimensional)
@@ -479,6 +488,7 @@ class MultiDimensionalRNN(MultiDimensionalRNNBase):
             return self.forward_one_directional_multi_dimensional_rnn(x)
 
     def _final_activation_function(self, final_activation_function_input):
+        # print("final_activation_function_input: " + str(final_activation_function_input))
         return self.fc3(final_activation_function_input)
 
     # Needs to be implemented in the subclasses