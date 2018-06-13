Fixed the problem with zero loss in  modules/train_multi_dimensional_rnn_ctc.py. The problem occured because a wrong value for the "horizontal_reduction_factor" (now renamed to "width_reduction_factor") was  passed to WarpCTCLoss.compute_ctc_loss. As a consequence, a wrong value for the "real" length of the probabilities was computed by the function WarpCTCLoss.get_real_probabilities_length  . This error in turn happened, because now with a network with three layer pairs (pairs of
BlockMDLSTM + BlockStridedConvolution), with block sizes for the BlockStridedConvolution of
(4,2) the reduction factor is 2 * 2 * 2 instead of 2 * 2 (for 2 layer pairs).
The error has been fixed now in a more robust way, by adding a new function "get_width_reduction_factor"
to "BlockMultiDimensionalLSTMLayerPairStacking" which properly computes the reduction factor, and
querying this function.

Additionally, a check has been added to the function WarpCTCLossInterface.get_real_probabilities_length,
which checks that the computed "real" probabilities length is not bigger than the probabilities_tensor
sequence length, which should never happen, and is indicative of an erroneous "width_reduction_factor".

Note that the computation of these real length using the "width_reduction_factor" is not as pretty,
as one would like. There is however not really another good way to do it, since the data loader /
data set simply does not know yet what network the data will be used for during creation,
and the downstream network, by reducing the width, will determine the
output "real" lengths, which are what matters for the WarpCTCLossInterface.compute_ctc_loss function.

	modified:   ctc_loss/warp_ctc_loss_interface.py
	modified:   modules/block_multi_dimensional_lstm_layer_pair_stacking.py
	modified:   modules/block_strided_convolution.py
	modified:   modules/multi_dimensional_rnn.py
	modified:   modules/train_multi_dimensional_rnn_ctc.py
	modified:   util/tensor_utils.py

ctc_loss/warp_ctc_loss_interface.py
@@ -2,6 +2,7 @@ import warpctc_pytorch
 import torch.tensor
 from util.utils import Utils
 from torch.autograd import Variable
+import util.tensor_utils
 
 
 class WarpCTCLossInterface:
@@ -45,8 +46,22 @@ class WarpCTCLossInterface:
         return result
 
     @staticmethod
-    def get_real_probabilities_length(label_row_tensor_slice, horizontal_reduction_factor: int):
+    def get_real_probabilities_length(label_row_tensor_slice, horizontal_reduction_factor: int,
+                                      probabilities_tensor_sequence_length: int):
         result = -(label_row_tensor_slice[label_row_tensor_slice.size(0) - 2] / horizontal_reduction_factor)
+
+        # Check that the computed result makes sense: it should not be larger than the
+        # probabilities_tensor_sequence_length, otherwise the result of warp_ctc_loss
+        # will become undefined, as it would lead to specifying a longer sequence than
+        # is actually available in the probabilities_tensor
+        if result > probabilities_tensor_sequence_length:
+            raise RuntimeError("Error: warp_ctc_loss_interface." +
+                               "get_real_probabilities_length -" +
+                               "the computed sequence length is bigger than the probabilities_tensor_sequence_length" +
+                               " possibly the horizontal_reduction_factor (" + str(horizontal_reduction_factor) + ")"
+                               + "is not correct?"
+                               )
+
         return result
 
     @staticmethod
@@ -75,17 +90,19 @@ class WarpCTCLossInterface:
 
     @staticmethod
     def create_probabilities_lengths_specification_tensor_different_lengths(labels_row_tensor,
-                                                                            horizontal_reduction_factor: int
+                                                                            horizontal_reduction_factor: int,
+                                                                            probabilities_tensor_sequence_length: int
                                                                             ):
         number_of_examples = labels_row_tensor.size(0)
-        probabilities_sequence_length_index = labels_row_tensor.size(1) - 2
-        # print("probabilities_sequence_length_index: " + str(probabilities_sequence_length_index))
         sequence_length = WarpCTCLossInterface.get_real_probabilities_length(labels_row_tensor[0],
-                                                                             horizontal_reduction_factor)
+                                                                             horizontal_reduction_factor,
+                                                                             probabilities_tensor_sequence_length)
         result = torch.IntTensor([sequence_length])
         for i in range(1, number_of_examples):
             sequence_length = WarpCTCLossInterface.get_real_probabilities_length(labels_row_tensor[i],
-                                                                                 horizontal_reduction_factor)
+                                                                                 horizontal_reduction_factor,
+                                                                                 probabilities_tensor_sequence_length)
+
             # print("probabilities sequence length: " + str(sequence_length))
             result = torch.cat((result, torch.IntTensor([sequence_length])), 0)
 
@@ -137,16 +154,12 @@ class WarpCTCLossInterface:
 
         return loss
 
-    # Computes the ctc_loss for a probabilities tensor of dimensions:
-    # 0: batch size, 1: sequence length, 2: number of symbol types + 1 (for blank)
-
-    def compute_ctc_loss(self, probabilities, labels_row_tensor, batch_size: int,
-                         horizontal_reduction_factor: int):
-
-        number_of_labels = labels_row_tensor.view(-1).size(0)
-        non_zeros = torch.nonzero(labels_row_tensor.view(-1).data).squeeze()
-        number_of_zero_labels = number_of_labels - non_zeros.size(0)
-
+    @staticmethod
+    def check_labels_row_tensor_contains_no_zeros(labels_row_tensor):
+        number_of_zero_labels = util.tensor_utils.TensorUtils.number_of_zeros(labels_row_tensor)
+        print(
+            "WarpCTCLossInterface.check_labels_row_tensor_contains_no_zeros - number_of_zeros: "
+            + str(number_of_zero_labels))
         # A sanity check to make sure the labels_row_tensor does not contain zeros,
         # which was an error in past usage
         if number_of_zero_labels != 0:
@@ -154,6 +167,18 @@ class WarpCTCLossInterface:
                                " only non-zero labels are allowed, since the 0 " +
                                "label is reserved for blanks - labels_row_tensor: " +
                                str(labels_row_tensor))
+
+    # Computes the ctc_loss for a probabilities tensor of dimensions:
+    # 0: batch size, 1: sequence length, 2: number of symbol types + 1 (for blank)
+    # width_reduction_factor: the factor by which the network reduces the original
+    # input width. This factor is used to compute the "real" portion of the network
+    # output given the information about the original real input widths in the
+    # labels_row_tensor
+    def compute_ctc_loss(self, probabilities, labels_row_tensor, batch_size: int,
+                         width_reduction_factor: int):
+
+        probabilities_tensor_sequence_length = probabilities.size(1)
+        WarpCTCLossInterface.check_labels_row_tensor_contains_no_zeros(labels_row_tensor)
 
         labels = Variable(WarpCTCLossInterface.
                           create_one_dimensional_labels_tensor_removing_padding_labels(labels_row_tensor))
@@ -167,7 +192,7 @@ class WarpCTCLossInterface:
         probabilities_sizes = \
             Variable(WarpCTCLossInterface.
                      create_probabilities_lengths_specification_tensor_different_lengths(
-                        labels_row_tensor, horizontal_reduction_factor))
+                        labels_row_tensor, width_reduction_factor, probabilities_tensor_sequence_length))
 
         # The ctc_loss interface expects the second dimension to be the batch size,
         # so the first and second dimension must be swapped
@@ -219,6 +244,10 @@ class WarpCTCLossInterface:
                                "should equal batch_size " + str(batch_size) + " but is " +
                                str(probabilities_batch_second_dimension.size(1))
                                )
+
+        # print("compute_ctc_loss - probabilities_sizes: " + str(probabilities_sizes))
+        # print("compute_ctc_loss - labels: " + str(labels))
+        # print("compute_ctc_loss - label_sizes: " + str(label_sizes))
 
         loss = self.ctc_loss(probabilities_batch_second_dimension, labels, probabilities_sizes, label_sizes)
 

modules/multi_dimensional_rnn.py
@@ -190,7 +190,7 @@ class MultiDimensionalRNNToSingleClassNetwork(torch.nn.Module):
 class NetworkToSoftMaxNetwork(torch.nn.Module):
     def __init__(self, network, input_size: SizeTwoDimensional, number_of_classes_excluding_blank: int):
         super(NetworkToSoftMaxNetwork, self).__init__()
-        self.multi_dimensional_rnn = network
+        self.network = network
         self.input_size = input_size
         self.number_of_output_channels = network.get_number_of_output_channels()
         self.number_of_classes_excluding_blank = number_of_classes_excluding_blank
@@ -214,10 +214,10 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
         return self.number_of_classes_excluding_blank + 1
 
     def set_training(self, training):
-        self.multi_dimensional_rnn.set_training(training)
+        self.network.set_training(training)
 
     def forward(self, x):
-        activations = self.multi_dimensional_rnn(x)
+        activations = self.network(x)
         batch_size = activations.size(0)
         # print(">>> activations.size(): " + str(activations.size()))
         # print("activations: " + str(activations))
@@ -272,6 +272,11 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
         # print(">>> MultiDimensionalRNNToSoftMaxNetwork.forward - soft_max_activations_summed: " + str(soft_max_activations_summed))
 
         return result
+
+    # Get the factor by which the original input width is reduced in the output
+    # of the network
+    def get_width_reduction_factor(self):
+        return self.network.get_width_reduction_factor()
 
 
 class MultiDimensionalRNNBase(torch.nn.Module):