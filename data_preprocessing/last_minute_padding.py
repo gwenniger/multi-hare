Fixed a bug in the code for the forward computation in modules/network_to_softmax_network.py in case minimal horizontal padding was used. The restructuring of the activations into a 2D structure with dimensions: number of activations, number of channels was not done correctly. Because of this the results when training with the minimal horizontal padding were suboptimal.
Added code for last minute padding. This is used when
the proper MDLSTM as opposed to the block-MDLSTM is
being used. In this case, considerable savings can be
made by padding seperately for every batch rather than
uniformly to the maximal dimensions that appear in
the entire training data.

Also improved the structure of the code, extracting
functions in very large methods in modules/network_to_softmax_network.py
and extracting a separate class  modules/module_io_structuring.py
for the specific structuring of input and output to modules in case
of block-chunking, as used for efficient computation with minimal
padding with block_mdlstm.

	modified:   data_preprocessing/iam_database_preprocessing/iam_dataset.py
	modified:   data_preprocessing/last_minute_padding.py
	modified:   data_preprocessing/load_mnist.py
	new file:   modules/module_io_structuring.py
	modified:   modules/network_to_softmax_network.py
	modified:   modules/train_multi_dimensional_rnn_ctc.py
	modified:   modules/trainer.py
	new file:   util/test_tensor_operations.py

data_preprocessing/last_minute_padding.py
@@ -69,21 +69,21 @@ class LastMinutePadding:
 
         percentage_padding_pixels = (float(total_padding_pixels) / total_pixels) * 100
         percentage_real_pixels = (float(total_real_pixels) / total_pixels) * 100
-        print("batch-padded images height, width: " + str(required_height) + "," + str(required_width))
-        print("percentage real pixels: " + str(percentage_real_pixels))
+        # print("batch-padded images height, width: " + str(required_height) + "," + str(required_width))
+        # print("percentage real pixels: " + str(percentage_real_pixels))
         print("percentage padding pixels: " + str(percentage_padding_pixels))
 
-        return image_tensors_padded_and_unsqueezed
+        return image_tensors_padded_and_unsqueezed, required_width
 
     # Pad a list of examples (3-D tensors) based on the maximum height and with
     # within the examples and the constraint that the height and width
     # must be dividable by height_required_per_network_row and and
     # width_required_per_network_output_column respectively
     def pad_and_cat_list_of_examples(self, image_tensor_list):
-        print("Performing last minute padding and concatenation of the provided examples for this batch...")
-        image_tensors_padded_and_unsqueezed = self.pad_and_unsqueeze_list_of_examples(image_tensor_list)
+        # print("Performing last minute padding and concatenation of the provided examples for this batch...")
+        image_tensors_padded_and_unsqueezed, required_width = self.pad_and_unsqueeze_list_of_examples(image_tensor_list)
         concatenated_padded_examples = torch.cat(image_tensors_padded_and_unsqueezed, 0)
-        return concatenated_padded_examples
+        return concatenated_padded_examples, required_width
 
 
 

modules/train_multi_dimensional_rnn_ctc.py
@@ -387,7 +387,7 @@ def create_model(checkpoint, data_height: int, input_channels: int, hidden_state
                 compute_multi_directional, clamp_gradients, use_dropout, opt.use_bias_in_block_strided_convolution)
 
     else:
-        raise RuntimeError("Error: unrecognized dataset name")
+        raise RuntimeError("Error: \"" + str(data_set_name) + "\" is an unrecognized dataset name")
 
     # See: https://pytorch.org/tutorials/beginner/former_torchies/nn_tutorial.html
     # multi_dimensional_rnn.register_backward_hook(printgradnorm)
@@ -585,8 +585,8 @@ def train_mdrnn_ctc(model_opt, checkpoint, train_loader, validation_loader, test
     # device_ids should include device!
     # device_ids lists all the gpus that may be used for parallelization
     # device is the initial device the model will be put on
-    device_ids = [0, 1]
-    # device_ids = [0]
+    # device_ids = [0, 1]
+    device_ids = [0]
 
     # See: https://pytorch.org/tutorials/beginner/former_torchies/nn_tutorial.html
     # multi_dimensional_rnn.register_backward_hook(printgradnorm)
@@ -722,7 +722,7 @@ def mnist_recognition_fixed_length():
 
 
 def mnist_recognition_variable_length(model_opt, checkpoint):
-    batch_size = 256
+    batch_size = 128
     min_num_digits = 1
     max_num_digits = 3
     # In MNIST there are the digits 0-9, and we also add a symbol for blanks
@@ -760,9 +760,14 @@ def mnist_recognition_variable_length(model_opt, checkpoint):
     #with torch.autograd.profiler.profile(use_cuda=False) as prof:
     blank_symbol = "_"
     image_input_is_unsigned_int = False
-    train_mdrnn_ctc(model_opt, checkpoint, train_loader, test_loader, test_loader, input_channels, input_size, hidden_states_size, batch_size,
+    use_block_mdlstm = True
+    train_mdrnn_ctc(model_opt, checkpoint, train_loader, test_loader,
+                    test_loader, input_channels,
+                    hidden_states_size, batch_size,
                     compute_multi_directional, use_dropout, vocab_list, blank_symbol,
-                    image_input_is_unsigned_int, "MNIST", minimize_horizontal_padding)
+                    image_input_is_unsigned_int, "MNIST", minimize_horizontal_padding,
+                    use_block_mdlstm)
+
     #print(prof)
 
 
@@ -836,7 +841,7 @@ def iam_word_recognition(model_opt, checkpoint):
     # With the improved padding, the height of the images is 128,
     # and memory usage is less, so batch_size 30 instead of 20 is possible,
     # but it is only slightly faster (GPU usage appears to be already maxed out)
-    batch_size = 128 #32 #128
+    batch_size = 64  #128 #32 #128
 
     # lines_file_path = "/datastore/data/iam-database/ascii/lines.txt"
     lines_file_path = model_opt.iam_database_lines_file_path
@@ -917,15 +922,15 @@ def main():
         model_opt = opt
 
     # mnist_recognition_fixed_length()
-    # mnist_recognition_variable_length(model_opt, checkpoint,)
+    mnist_recognition_variable_length(model_opt, checkpoint,)
 
-    if opt.iam_database_data_type == "lines":
-        iam_line_recognition(model_opt, checkpoint)
-    elif opt.iam_database_data_type == "words":
-        iam_word_recognition(model_opt, checkpoint)
-    else:
-        raise RuntimeError("Unrecognized data type")
-    # #cifar_ten_basic_recognition()
+    # if opt.iam_database_data_type == "lines":
+    #     iam_line_recognition(model_opt, checkpoint)
+    # elif opt.iam_database_data_type == "words":
+    #     iam_word_recognition(model_opt, checkpoint)
+    # else:
+    #     raise RuntimeError("Unrecognized data type")
+    # # #cifar_ten_basic_recognition()
 
 
 if __name__ == "__main__":