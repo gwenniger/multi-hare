Nearly completely fixed the problems with nan gradient norms when performing only gradient clamping and norm-based gradient clipping and no additional global value-based gradient clipping. The fix is to apply gradient clamping even for simple multiplications, e.g. when computing the multiple of a state and a gate output. Appearantly even these multiplications can give rise to exploding gradients when not clamped.
For some reason at the start of training there is still often an
example with nan gradient-norm value. This is odd, as it seems that
gradient clamping is now added everywhere, including throughout the
MDLSTM, in the block_strided_convolution and in the network_to_softmax
layer. While the current situation is already a big improvment, and
probably workable for learning, one would find the cause of and then
eliminate these last nans as well.

	new file:   modules/find_bad_gradients.py
	modified:   modules/inside_model_gradient_clipping.py
	modified:   modules/multi_dimensional_lstm.py
	modified:   modules/network_to_softmax_network.py
	modified:   modules/optim.py
	modified:   modules/parallel_multiple_input_convolutions_computation.py
	modified:   modules/train_multi_dimensional_rnn_ctc.py
	modified:   modules/trainer.py

modules/multi_dimensional_lstm.py
@@ -234,9 +234,9 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
 
             input_and_input_gate_combined = torch.mul(input_activation_column, input_gate_activation_column)
 
-            # if self.clamp_gradients:
-            #     input_and_input_gate_combined = \
-            #         InsideModelGradientClamping.register_gradient_clamping(input_and_input_gate_combined)
+            if self.clamp_gradients:
+                input_and_input_gate_combined = \
+                    InsideModelGradientClamping.register_gradient_clamping(input_and_input_gate_combined, 10, False)
 
 
             # print("input and input gate combined: " + str(input_and_input_gate_combined))
@@ -264,10 +264,10 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
                 torch.mul(forget_gate_one_activation_column,
                           memory_states_column_forget_gate_one)
 
-            # if self.clamp_gradients:
-            #     forget_gate_one_activation_multiplied_with_previous_memory_state = \
-            #         InsideModelGradientClamping.register_gradient_clamping(
-            #             forget_gate_one_activation_multiplied_with_previous_memory_state)
+            if self.clamp_gradients:
+                forget_gate_one_activation_multiplied_with_previous_memory_state = \
+                    InsideModelGradientClamping.register_gradient_clamping(
+                        forget_gate_one_activation_multiplied_with_previous_memory_state, 10, False)
 
             memory_states_column_forget_gate_two = StateUpdateBlock.\
                 get_shifted_column_fast(previous_memory_state_column)
@@ -295,10 +295,10 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
             forget_gate_two_activation_multiplied_with_previous_memory_state = torch.mul(
                 forget_gate_two_activation_column, memory_states_column_forget_gate_two)
 
-            # if self.clamp_gradients:
-            #     forget_gate_two_activation_multiplied_with_previous_memory_state = \
-            #         InsideModelGradientClamping.register_gradient_clamping(
-            #             forget_gate_two_activation_multiplied_with_previous_memory_state)
+            if self.clamp_gradients:
+                forget_gate_two_activation_multiplied_with_previous_memory_state = \
+                    InsideModelGradientClamping.register_gradient_clamping(
+                        forget_gate_two_activation_multiplied_with_previous_memory_state, 10, False)
 
             # print("input_and_input_gate_combined: " + str(input_and_input_gate_combined))
 
@@ -351,6 +351,9 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
             # This is following the deep learning book
             activation_column = torch.mul(new_memory_state, output_gate_activation_column)
 
+            if self.clamp_gradients:
+                InsideModelGradientClamping.register_gradient_clamping(activation_column, 10, False)
+
             #activation_column = self.get_activation_function()(input_state_plus_input)
             # activation_column = new_memory_state_activation_column
             # print("output gate activation column: " + str(output_gate_activation_column))
@@ -358,6 +361,7 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
 
             previous_hidden_state_column = activation_column
             previous_memory_state_column = new_memory_state
+
             activations.append(activation_column)
 
             # In the loop the value of grad_fn becomes set, as a backwards path for
@@ -442,7 +446,8 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
                                                        previous_memory_state_column)
 
         if self.clamp_gradients:
-            output_gate_memory_state_column = InsideModelGradientClamping.register_gradient_clamping_default_clamping_bound(output_gate_memory_state_column)
+            output_gate_memory_state_column = InsideModelGradientClamping.\
+                register_gradient_clamping_default_clamping_bound(output_gate_memory_state_column)
 
         return self.compute_weighted_input_forget_gate(
                 mdlstm_parameters.get_output_gate_hidden_state_column(),

modules/network_to_softmax_network.py
@@ -104,7 +104,7 @@ class KeepAllActivationsResizer(ActivationsResizer):
 # of dimension: batch_size * number_of_output_channels * number_of_classes
 class NetworkToSoftMaxNetwork(torch.nn.Module):
 
-    LINEAR_LAYER_GRADIENT_CLAMPING_BOUND = 100
+    LINEAR_LAYER_GRADIENT_CLAMPING_BOUND = 10
 
     def __init__(self, network,
                  number_of_classes_excluding_blank: int,
@@ -377,7 +377,8 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
             # print("NetworkToSoftMaxNetwork - register gradient clamping...")
             class_activations = InsideModelGradientClamping.\
                 register_gradient_clamping(class_activations,
-                                           NetworkToSoftMaxNetwork.LINEAR_LAYER_GRADIENT_CLAMPING_BOUND)
+                                           NetworkToSoftMaxNetwork.LINEAR_LAYER_GRADIENT_CLAMPING_BOUND,
+                                           False)
 
         # print("class_activations: " + str(class_activations))
         if self.input_is_list and self.use_block_mdlstm:
@@ -408,7 +409,8 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
             # print("class_activations.size(): " + str(class_activations.size()))
             class_activations = NetworkToSoftMaxNetwork.\
                 get_class_activations_summed_over_height_from_2d_activations_tensor(class_activations,
-                                                                                    number_of_activation_rows)
+                                                                                    number_of_activation_rows,
+                                                                                    self.clamp_gradients)
             class_activations_resized = class_activations.view(batch_size, -1,
                                                                self.get_number_of_classes_including_blank())
 
@@ -449,7 +451,9 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
     # tensor, that is, the class activations have been computed from padded activation
     # tensors
     @staticmethod
-    def get_class_activations_summed_over_height_from_2d_activations_tensor(class_activations, number_of_activation_rows):
+    def get_class_activations_summed_over_height_from_2d_activations_tensor(class_activations,
+                                                                            number_of_activation_rows,
+                                                                            clamp_gradients):
         # print("get_class_activations_summed_over_height - number_of_activation_rows: "
         # + str(number_of_activation_rows))
         # print("get_class_activations_summed_over_height - input.size(): " + str(class_activations.size()))
@@ -457,6 +461,11 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
         class_activations_with_height = class_activations.view(columns_after_height_extraction,
                                                                number_of_activation_rows, -1)
         result = torch.sum(class_activations_with_height, 1)
+        if clamp_gradients:
+            InsideModelGradientClamping.\
+                register_gradient_clamping(result,
+                                           NetworkToSoftMaxNetwork.LINEAR_LAYER_GRADIENT_CLAMPING_BOUND,
+                                           False)
         # print("get_class_activations_summed_over_height - result.size(): " + str(result.size()))
         return result
 

modules/trainer.py
@@ -11,6 +11,8 @@ from modules.gradient_clipping import GradientClipping
 import torch.nn
 from modules.validation_stats import ValidationStats
 from modules.optim import Optim
+import modules.find_bad_gradients
+from graphviz import render
 
 
 class ModelProperties:
@@ -179,12 +181,20 @@ class Trainer:
             else:
                 loss_value = loss.item()
 
-            # print("loss: " + str(loss))
+            print("loss: " + str(loss))
             # loss = criterion(outputs, labels)
 
             time_start_loss_backward = util.timing.date_time_start()
+
+            # get_dot = modules.find_bad_gradients.register_hooks(outputs)
             loss.backward()
+
+            # dot = get_dot()
+            # dot.save('mdlst_find_bad_gradients.dot')
+            # render('dot', 'png', 'mdlst_find_bad_gradients.dot')
             # print("Time used for loss backward: " + str(util.timing.milliseconds_since(time_start_loss_backward)))
+
+            # raise RuntimeError("stopping after find bad gradients")
 
             # Perform step including gradient clipping
             made_gradient_norm_based_correction, total_norm = self.optimizer.step()