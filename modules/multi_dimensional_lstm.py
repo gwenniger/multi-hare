Made a fix to the Leaky LP cell computation, following the diagram of the Leaky LP cell from the Leifert tecnical report "A new Approach for Cells in Multidimensional Recurrent Neural Networks"  which includes a diagram of the computational graph of Leaky LP cells. (http://ftp.math.uni-rostock.de/pub/preprint/2012/pre12_04.pdf).
The Leaky LP cell has two output gates, which must take the newly compute memory
state as input, as opposed to the previous memory state (which was used
till now).  This "bug" was gone unnoticed, since even the previous implementation
before this fix was giving really good results. Perhaps this is a minor difference,
that does not really influence the quality of the results. This remains to be seen.

Note that this also is closer to the original MDLSTM computational structure, which also
uses the new memory state (as opposed to previous memory state) for computing the output
gate activation.

	modified:   modules/multi_dimensional_lstm.py

modules/multi_dimensional_lstm.py
@@ -809,9 +809,14 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
                 torch.mul(input_activation_column, input_and_states_lambda_gate_activation_column_input) +\
                 torch.mul(states_lambda_gate_reweighted_memory_states,
                           input_and_states_lambda_gate_activation_column_states)
-
+            
+            # The output gates memory state colum is computed using the new memory state as opposed to 
+            # previous memory state column. This is following the Leifert technical report, which shows a digram of 
+            # the Leaky LP cell. See: http://ftp.math.uni-rostock.de/pub/preprint/2012/pre12_04.pdf
+            # This is a fix, earlier was uing "previous_memory_state_column" here, which was also giving decent results.
             output_gates_memory_state_column = \
-                mdlstm_parameters.compute_output_gate_memory_state_weighted_input(previous_memory_state_column)
+                mdlstm_parameters.compute_output_gate_memory_state_weighted_input(new_memory_state)
+
             # print(">>>> output_gates_memory_state_column: " + str(output_gates_memory_state_column))
             # print(">>>> output_gates_memory_state_column.size(): " + str(output_gates_memory_state_column.size()))
             output_gates_memory_state_columns = torch.chunk(output_gates_memory_state_column, 2, 1)