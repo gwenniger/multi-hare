Fixed the combination of multi-directional computation and no examples packing, which was not working previously. This required changes in network_to_softmax_network, which was not resizing the activations in the right way to form the 2D input to the convolution, in case multiple directions were used without packing. It furthermore required changes in multi_dimensional_lstm, preparing a four-directional input, as was done previously only when examples packing was being used.
This fix now should allow to run the network with four-directional computation and with
or without examples packing, allowing to compare the runtimes of both, and thereby
determine the speed benefit of using examples packing. Note that the speed benefits
are mainly caused by saving out memory with examples packing, allowing a larger
batch size to be used with the same memory and thereby increasing speed. For the
comparison both models (with or without examples packing) should be ran with the maximum
batch size that still works for that setting.

	modified:   modules/block_strided_convolution.py
	modified:   modules/mdlstm_examples_packing.py
	modified:   modules/multi_dimensional_lstm.py
	modified:   modules/multi_dimensional_lstm_parameters.py
	modified:   modules/network_to_softmax_network.py

modules/block_strided_convolution.py
@@ -144,6 +144,12 @@ class BlockStridedConvolution(Module):
         # result = InsideModelGradientClamping.register_gradient_clamping(result)
         return result
 
+    @staticmethod
+    def chunk_four_parts_on_channel_dimension_and_sum(tensor):
+        results_per_direction = torch.chunk(tensor, 4, 1)
+        result = torch.sum(torch.stack(results_per_direction, 0), 0)
+        return result
+
     def compute_forward_from_chunked_input(self, x_chunked, tensor_list_chunking):
 
         result = self.compute_forward_one_directional(x_chunked)
@@ -164,8 +170,7 @@ class BlockStridedConvolution(Module):
         # If the weights are shared across directions, this summation has already been done over the inputs
         # before computing the convolution
         if self.compute_multi_directional and not self.share_weights_across_directions:
-            results_per_direction = torch.chunk(result, 4, 1)
-            result = torch.sum(torch.stack(results_per_direction, 0), 0)
+            result = BlockStridedConvolution.chunk_four_parts_on_channel_dimension_and_sum(result)
             # result = TensorUtils.sum_list_of_tensors(results_per_direction)
 
         if self.use_example_packing:
@@ -230,6 +235,16 @@ class BlockStridedConvolution(Module):
         #     check_dechunking_chunked_tensor_list_recovers_original(tensor_list_chunking, x_three_dim, x_chunked)
         return x_chunked, tensor_list_chunking
 
+    @staticmethod
+    def sum_channels_four_directions(x_chunked):
+        # print("x_chunked.size(): " + str(x_chunked.size()))
+        x_chunked_split_by_channels = torch.chunk(x_chunked, 4, 1)
+        stacked_channels = torch.stack(x_chunked_split_by_channels, 0)
+        # print("stacked_channels.size(): " + str(stacked_channels.size()))
+        x_chunked_summed_channels = torch.sum(stacked_channels, 0)
+        # print("x_chunked_summed_channels.size(): " + str(x_chunked_summed_channels.size()))
+        return x_chunked_summed_channels
+
     def forward(self, x):
 
         """
@@ -258,12 +273,7 @@ class BlockStridedConvolution(Module):
             if self.compute_multi_directional:
 
                 if self.share_weights_across_directions:
-                    # print("x_chunked.size(): " + str(x_chunked.size()))
-                    x_chunked_split_by_channels = torch.chunk(x_chunked, 4, 1)
-                    stacked_channels = torch.stack(x_chunked_split_by_channels, 0)
-                    # print("stacked_channels.size(): " + str(stacked_channels.size()))
-                    x_chunked_summed_channels = torch.sum(stacked_channels, 0)
-                    # print("x_chunked_summed_channels.size(): " + str(x_chunked_summed_channels.size()))
+                    x_chunked_summed_channels = BlockStridedConvolution.sum_channels_four_directions(x_chunked)
                     activations_summed = self.compute_forward_from_chunked_input(
                         x_chunked_summed_channels, tensor_list_chunking)
 
@@ -297,7 +307,16 @@ class BlockStridedConvolution(Module):
                     x_chunked, tensor_list_chunking)
                 return activations
         else:
-            return self.compute_forward_one_directional(x)
+            if self.compute_multi_directional:
+                if self.share_weights_across_directions:
+                    x_summed_channels = BlockStridedConvolution.sum_channels_four_directions(x)
+                    return self.compute_forward_one_directional(x_summed_channels)
+                else:
+                    result = self.compute_forward_one_directional(x)
+                    result = BlockStridedConvolution.chunk_four_parts_on_channel_dimension_and_sum(result)
+                    return result
+            else:
+                return self.compute_forward_one_directional(x)
 
     def get_width_reduction_factor(self):
         return self.block_size.width

modules/multi_dimensional_lstm.py
@@ -292,8 +292,17 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
                     create_vertically_and_horizontally_packed_examples_and_mask_one_direction(examples)
                 number_of_images = 1
         else:
-            # Create a binary mask that tells which of the cell positions are valid and which are not
-            skewed_images_variable = ImageInputTransformer.create_skewed_images_variable_four_dim(examples)
+            if self.compute_multi_directional():
+                skewed_images_variable_direction_one = \
+                    ImageInputTransformer.create_skewed_images_variable_four_dim(examples)
+                tensor_flippings = MDLSTMExamplesPacking.create_four_directions_tensor_flippings()
+                skewed_images_variable = MDLSTMExamplesPacking.\
+                    create_multi_directional_examples_stacked_on_channel_direction(
+                        skewed_images_variable_direction_one, tensor_flippings)
+            else:
+                # Create a binary mask that tells which of the cell positions are valid and which are not
+                skewed_images_variable = ImageInputTransformer.create_skewed_images_variable_four_dim(examples)
+
             mask = ImageInputTransformer.create_skewed_images_mask_two_dim(examples)
             number_of_images = examples.size(0)
             mdlstm_examples_packing = None
@@ -814,10 +823,10 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
             # previous memory state column. This is following the Leifert technical report, which shows a digram of 
             # the Leaky LP cell. See: http://ftp.math.uni-rostock.de/pub/preprint/2012/pre12_04.pdf
             # This is a fix, earlier was uing "previous_memory_state_column" here, which was also giving decent results.
-            # output_gates_memory_state_column = \
-            #    mdlstm_parameters.compute_output_gate_memory_state_weighted_input(new_memory_state)
             output_gates_memory_state_column = \
-                mdlstm_parameters.compute_output_gate_memory_state_weighted_input(previous_memory_state_column)
+                mdlstm_parameters.compute_output_gate_memory_state_weighted_input(new_memory_state)
+            # output_gates_memory_state_column = \
+            #    mdlstm_parameters.compute_output_gate_memory_state_weighted_input(previous_memory_state_column)
             # print(">>>> output_gates_memory_state_column: " + str(output_gates_memory_state_column))
             # print(">>>> output_gates_memory_state_column.size(): " + str(output_gates_memory_state_column.size()))
             output_gates_memory_state_columns = torch.chunk(output_gates_memory_state_column, 2, 1)
@@ -963,7 +972,7 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
         #       + str(util.timing.milliseconds_since(time_start_network_forward)))
 
         # for i, element in enumerate(result):
-        #     print("MDLSTM - result[" + str(i) + "].size(): " + str(element.size()))
+        #    print("MDLSTM - result[" + str(i) + "].size(): " + str(element.size()))
 
         # Dropout is applied to the output of the MDLSTM layer, as in the paper
         # "Dropout improves Recurrent Neural Networks for Handwriting Recognition"
@@ -971,6 +980,10 @@ class MultiDimensionalLSTM(MultiDimensionalRNNBase):
             for example_result in result:
                 # Dropout is done in place, to save memory
                 F.dropout(example_result, p=0.5, training=self.training, inplace=True)
+
+        if self.compute_multi_directional() and not self.use_example_packing:
+            # Concatenate the list of 3D tensors in result on the zeroth dimension
+            result = torch.stack(result, 0)
 
         return result
 

modules/multi_dimensional_lstm_parameters.py
@@ -1479,9 +1479,15 @@ class MultiDirectionalMultiDimensionalLSTMParametersFullyParallel(
         #      + str(self.bladie_input_column_index))
         input_column = self.skewed_images_variable[:, :, :, self.next_input_column_index]
 
-        # print("prepare_computation_next_column_functions - input_column: " + str(input_column))
+        if input_column.size(1) < self.number_of_directions:
+            raise RuntimeError("Error: input_column.size(1) = " + str(input_column.size(1)) +
+                               ", but it should be a multiple of self.number_of_directions = " +
+                               str(self.number_of_directions))
+        # print("prepare_computation_next_column_functions - input_column.size(): " + str(input_column.size()))
         input_columns_split_by_direction = torch.chunk(input_column,
                                                        self.number_of_directions, 1)
+        # print(">>> self.number_of_directions: " + str(self.number_of_directions))
+        # print(">>> len(input_columns_split_by_direction): " + str(len(input_columns_split_by_direction)))
 
         computation_arguments_list = list([])
         for i in range(0, self.number_of_directions):

modules/network_to_softmax_network.py
@@ -427,7 +427,6 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
         return NetworkToSoftMaxNetwork.get_activations_single_tensor_and_activation_heights_and_widths(
             activations, multiple_output_directions)
 
-
     @staticmethod
     def resize_activations_block_mdlstm_minimal_padding(activations):
         # In this case there is no batch dimension, activations has three dimensions:
@@ -451,8 +450,13 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
 
         activations_with_swapped_channels_and_width = activations_height_removed.transpose(1, 2)
         # Change view to remove the batch dimension
+        # activations_resized_no_batch_dimension = activations_with_swapped_channels_and_width.contiguous(). \
+        #    view(-1, self.number_of_output_channels)
+        # Use activations.size(1) to determine number of output channels, because self.number_of_output_channels
+        # does not include the directions so gives the wrong number here
+        number_of_output_channels = activations.size(1)
         activations_resized_no_batch_dimension = activations_with_swapped_channels_and_width.contiguous(). \
-            view(-1, self.number_of_output_channels)
+            view(-1, number_of_output_channels)
         return activations_resized_no_batch_dimension
 
     @staticmethod
@@ -568,14 +572,16 @@ class NetworkToSoftMaxNetwork(torch.nn.Module):
 
         # Restructure the activations to be 2-dimensional, with the first dimension
         # the number of activations and the second dimension the number of channels
+        # print(">>> network_to_softmax_network: activations.size(): " +
+        #      str(activations.size()))
         if (self.use_block_mdlstm or self.use_example_packing) and self.input_is_list:
             activations_resized_two_dimensional = \
                 NetworkToSoftMaxNetwork.resize_activations_block_mdlstm_minimal_padding(activations)
         else:
             activations_resized_two_dimensional = self.resize_activations_padded_batch(activations)
 
-
-        # print("activations_resized_no_height: " + str(activations_resized_no_height))
+        # print(">>> network_to_softmax_network: activations_resized_two_dimensional.size(): " +
+        #      str(activations_resized_two_dimensional.size()))
         class_activations = self.fully_connected_layer(activations_resized_two_dimensional)
 
         if self.clamp_gradients: