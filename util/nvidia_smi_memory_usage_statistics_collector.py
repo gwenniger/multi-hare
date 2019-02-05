ed65806883f822d1a06e6e4f3bd475e007d5ed62
Gideon Maillette de Buy Wenniger <gideon@samurai-gtx1080ti.computing.dcu.ie>
2019-02-05 18:23:57 +0000

    Another fix for compatibility with python 3.5.
    	modified:   util/nvidia_smi_memory_usage_statistics_collector.py

--- util/nvidia_smi_memory_usage_statistics_collector.py
+++ util/nvidia_smi_memory_usage_statistics_collector.py
@@ -109,11 +109,18 @@ class NvidiaSmiMemoryStatisticsCollector:
             Keys are device ids as integers.
             Values are memory usage as integers in MB.
         """
+        # result = subprocess.check_output(
+        #    [
+        #        'nvidia-smi', '--query-gpu=memory.used',
+        #        '--format=csv,nounits,noheader'
+        #    ], encoding='utf-8')
+        
+        # For python 3.5
         result = subprocess.check_output(
             [
                 'nvidia-smi', '--query-gpu=memory.used',
                 '--format=csv,nounits,noheader'
-            ], encoding='utf-8')
+            ])
         # Convert lines into a dictionary
         gpu_memory = [int(x) for x in result.strip().split('\n')]
         gpu_memory_map = dict(zip(range(len(gpu_memory)), gpu_memory))
